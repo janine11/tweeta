6cdec57203c9b67214e674824a677ab595a5fa22
Janine Luongo <janineluongo@gmail.com>
2021-09-07 12:37:28 -0400

    added user registration

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// edited to /api/v1 bc we;re using React as front end it's an API route
+app.use('/api/v1/users', usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt')
+const db = require('../models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Register new user. */
+router.post('/register', function(req, res, next) {
+  // take username, password
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({
+      error: 'Please include username and password.'
+    })
+  }
+  // create a new user...
+  // check if username is already taken
+  // this returns a promise (once this has happened, do something else... here .then())
+  db.User.findOne({
+    where: {
+      username: req.body.username
+    }
+  })
+  .then((user) => {
+    if (user) {
+      res.status(400).json({
+        error: 'username already taken'
+      })
+      // return stops it
+      return
+    }
+    
+    // hash password
+    // hash is also a promise, 10 is the # of times we're encrypting
+    bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      // store in database
+      // create is also a promise
+      db.User.create({
+        // give it an object with details of thing we're creating
+        username: req.body.username,
+        password: hash
+      })
+      .then((user) => {
+        res.status(201).json({
+          success: 'User created.'
+        })
+      })
+    })
+    // respond with success or error 
+  })
 });
 
 module.exports = router;
